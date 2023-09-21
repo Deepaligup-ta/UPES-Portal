51b88f02b39c5ec139ba190408277416074a0598
Sanky2104 <guptasanskriti21@gmail.com>
2023-09-22 01:33:15 +0530

    modified:   src/Pages/Assessment.jsx 	new file:   src/Styles/Assessment.css 	modified:   src/components/faculty/Faculty.jsx

--- src/components/faculty/Faculty.jsx
+++ src/components/faculty/Faculty.jsx
@@ -13,60 +13,12 @@ import Timetable from "../../Pages/Timetable";
 
 const Faculty = () => {
 
-    const [clickDashboard, setClickedDashboard] = useState('false');
-    const [clickPolicies, setClickedPolicies] = useState('false');
-    const [clickTimetable, setClickedTimetable] = useState('false');
-    const [clickSyllabus, setClickedSyllabus] = useState('false');
-    const [clickAssessment, setClickedAssessment] = useState('false');
-    const [clickBroadcast, setClickedBroadcast] = useState('false');
-    const [clickCourses, setClickedCourses] = useState('false');
-    const [clickCalender, setClickedCalender] = useState('false');
-    const [clickAwardsheet, setClickedAwardsheet] = useState('false');
-    const handleDashboard = () => {
-        console.log('clicked');
-        setClickedDashboard(!clickDashboard)
-    }
+    const [click,setClick]= useState("dashboard");
 
-    const handleCalender = () => {
+    const handleClick = (prop)=>{
         console.log('clicked');
-        setClickedCalender(!clickCalender)
+        setClick(prop);
     }
-
-    const handleCourses = () => {
-        console.log('clicked');
-        setClickedCourses(!clickCourses)
-    }
-
-    const handleBroadcast = () => {
-        console.log('clicked');
-        setClickedBroadcast(!clickBroadcast)
-    }
-
-    const handleTimetable = () => {
-        console.log('clicked');
-        setClickedTimetable(!clickTimetable)
-    }
-
-    const handlePolicies = () => {
-        console.log('clicked');
-        setClickedPolicies(!clickPolicies)
-    }
-
-    const handleSyllabus = () => {
-        console.log('clicked');
-        setClickedSyllabus(!clickSyllabus)
-    }
-
-    const handleAssessment = () => {
-        console.log('clicked');
-        setClickedAssessment(!clickAssessment)
-    }
-
-    const handleAwardsheets = () => {
-        console.log('clicked');
-        setClickedAwardsheet(!clickAwardsheet)
-    }
-
     return (
         <div className="main">
             <div className="side-nav">
@@ -77,39 +29,39 @@ const Faculty = () => {
                 </div>
                 <div className="upper">
                     <div className="panels">
-                        <div className="panel" onClick={handleDashboard}>
+                        <div className="panel" onClick={()=>{handleClick("dashboard")}}>
                             <img src="./images/dashboard-logo.svg" alt="dashboard-logo" />
                             <label>Dashboard</label>
                         </div>
-                        <div className="panel" onClick={handleCalender}>
+                        <div className="panel" onClick={()=>{handleClick("calender")}}>
                             <img src="./images/calendar-logo.svg" alt="calendar logo" />
                             <h4 >Calendar</h4>
                         </div>
-                        <div className="panel" onClick={handleCourses}>
+                        <div className="panel" onClick={()=>{handleClick("courses")}}>
                             <img src="./images/courses-logo.svg" alt="courses logo" />
                             <h4>Courses</h4>
                         </div>
-                        <div className="panel"  onClick={handleBroadcast}>
+                        <div className="panel"  onClick={()=>{handleClick("broadcast")}}>
                             <img src="./images/broadcast.svg" alt="broadcast messages" />
                             <h4>Broadcast Messages</h4>
                         </div>
-                        <div className="panel" onClick={handleTimetable}>
+                        <div className="panel" onClick={()=>{handleClick("timetable")}}>
                             <img src="./images/timetable-logo.svg" alt="timetable logo" />
                             <h4>Timetable</h4>
                         </div>
-                        <div className="panel" onClick={handlePolicies}>
+                        <div className="panel" onClick={()=>{handleClick("policies")}}>
                             <img src="./images/policies-logo.svg" alt="policies" />
                             <h4>Policy</h4>
                         </div>
-                        <div className="panel" onClick={handleSyllabus}>
+                        <div className="panel" onClick={()=>{handleClick("syllabus")}}>
                             <img src="./images/syllabus-logo.svg" alt="syllabus" />
                             <h4>Syllabus</h4>
                         </div>
-                        <div className="panel" onClick={handleAssessment}>
+                        <div className="panel" onClick={()=>{handleClick("assessment")}}>
                             <img src="./images/internal_assessment.svg" alt="internal assessment" />
                             <h4>Internal Assessments</h4>
                         </div>
-                        <div className="panel" onClick={handleAwardsheets}>
+                        <div className="panel" onClick={()=>{handleClick("awardSheets")}}>
                             <img src="./images/award_sheets.svg" alt="award sheets" />
                             <h4>Award Sheets</h4>
                         </div>
@@ -125,15 +77,15 @@ const Faculty = () => {
 
 
             <div className="right-side">
-                {clickDashboard ? <Dashboard /> : false}
-                {clickPolicies ? <Policies /> : false}
-                {clickCalender ? <Calender /> : false}
-                {clickTimetable ? <Timetable /> : false}
-                {clickSyllabus ? <Syllabus /> : false}
-                {clickAwardsheet ? <Awardsheets /> : false}
-                {clickCourses ? <Courses /> : false}
-                {clickAssessment ? <Assessment /> : false}
-                {clickBroadcast ? <Broadcast /> : false}
+                {click === 'dashboard' && <Dashboard />}
+                {click === 'calender' && <Calender />}
+                {click === 'courses' && <Courses />}
+                {click === 'broadcast' && <Broadcast />}
+                {click === 'timetable' && <Timetable />}
+                {click === 'policies' && <Policies />}
+                {click === 'syllabus' && <Syllabus />}
+                {click === 'assessment' && <Assessment />}
+                {click === 'awardSheets' && <Awardsheets />}
 
             </div>
         </div>
