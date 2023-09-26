48c7aa6337cf4894d28bb765618d4e3d84c232cc
Sanky2104 <guptasanskriti21@gmail.com>
2023-09-26 16:07:05 +0530

    new file:   src/components/Management/Policies/Policies.css 	new file:   src/components/Management/Policies/Policies.jsx

---
+++ src/components/Management/Policies/Policies.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import Sidebar_faculty from '../components/SideBar/Sidebar-faculty'
+
+const Policies = () => {
+  return (
+    <div className='policies flex-start'>
+
+
+    
+
+
+
+
+
+      <Sidebar_faculty/>
+    </div>
+  )
+}
+
+export default Policies
