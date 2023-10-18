88f656a5434da11a03e49c0c9cb16c2743366c0f
bhupenderlost <s.bhupender2401@gmail.com>
2023-10-18 19:22:50 +0530

    .

--- middlewares/rateLimit.js
+++ middlewares/rateLimit.js
@@ -2,7 +2,7 @@ import { rateLimit } from 'express-rate-limit'
 
 export const rateLimiter = rateLimit({
 	windowMs: 1 * 60 * 1000, 
-	limit: process.env.RATE_LIMIT_REQUEST || 1000,
+	limit: process.env.RATE_LIMIT_REQUEST || 100,
 	standardHeaders: 'draft-7', 
 	legacyHeaders: false, 
 })
